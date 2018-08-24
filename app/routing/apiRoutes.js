a7a1cfbbfc1f99b8ab67c6cca15ddcdb6010226d
Michael LeGrange <mlegrange15@att.net>
2018-08-24 10:16:13 -0500

    added loops to apiRoutes for logic to calc score comparisons

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,18 +2,16 @@
 var friendsData = require("../data/friends");
 
 // Routing to api pages
-module.exports = function(app) {
+module.exports = function (app) {
 
-    // API GET /friends route request that will show JSON of all objects in the friendsData array
-  app.get("/api/friends", function(req, res) {
+  // API GET /friends route request that will show JSON of all objects in the friendsData array
+  app.get("/api/friends", function (req, res) {
     res.json(friendsData);
   });
 
   // API POST /friends route request that will take the incoming results from survey and handle logic of compatibility
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
 
-    
-  
     var newPerson = {
       name: req.body.name,
       photo: req.body.photo,
@@ -21,15 +19,31 @@ module.exports = function(app) {
     };
 
     var newPersonScore = [];
-    // console.log(req.body.scores);
-    
 
-    for(var i=0;i<req.body.scores.length;i++){
+    for (var i = 0; i < req.body.scores.length; i++) {
 
-      newPersonScore.push(parseInt(req.body.scores[i]));      
+      newPersonScore.push(parseInt(req.body.scores[i]));
     }
 
     newPerson.scores = newPersonScore;
+
+
+    var bestMatch = {
+      name: "",
+      photo: "",
+      scores: []
+    };
+
+    for (var i = 0; i < friendsData; i++) {
+
+      var scoreDifference = 0;
+
+      for (var j = 0; j < newPersonScore; j++) {
+          // Calc absolute value diffrence and store in scoreDifference
+
+      }
+
+    }
 
     console.log(newPerson);
 
